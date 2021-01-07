60aa1f6d6c462b2688b549021ff387e0fae2b457
LeeHoseop <ghtjq3909@naver.com>
2021-01-07 23:50:35 +0900

    Update main.html request

--- test.py
+++ test.py
@@ -4,10 +4,10 @@ import time
 tokenizer = AutoTokenizer.from_pretrained("laxya007/gpt2_Marketingman")
 model = AutoModelForCausalLM.from_pretrained("laxya007/gpt2_Marketingman")
 
-prompt = "Today, I introduce this lotion. This lotion is"
+prompt = "Hello, customer. My name is kent. In case I"
 
 input_ids = tokenizer.encode(prompt, return_tensors='pt')
-length = 20
+length = 200
 length += len(input_ids.tolist()[0])
 print(input_ids)
 print(len(input_ids.tolist()[0]))
@@ -18,9 +18,9 @@ s = time.time()
 sample_outputs = model.generate(input_ids, pad_token_id=50256,
                                 do_sample=True,
                                 max_length=length,
-                                min_length=length,
+                                min_length=int(length / 1.5),
                                 top_k=40,
-                                num_return_sequences=3)
+                                num_return_sequences=1)
 print("{}초 경과 -----------------------".format(time.time()-s))
 
 for i, sample_output in enumerate(sample_outputs):
